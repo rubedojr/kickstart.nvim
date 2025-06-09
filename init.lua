a2d9aeb4a38fe940ce73425591ecf22d1b3d57f9
Julio Valentin <rubedoje@gmail.com>
2025-06-09 09:07:42 -0400

    Removed 3rd party version of TS FSharp for Ionide definitions.

--- init.lua
+++ init.lua
@@ -873,16 +873,6 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
       local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-      parser_config.fsharp = {
-        install_info = {
-          url = 'https://github.com/ionide/tree-sitter-fsharp',
-          branch = 'main',
-          files = { 'src/scanner.c', 'src/parser.c' },
-          location = 'fsharp',
-        },
-        requires_generate_from_grammar = false,
-        filetype = 'fsharp',
-      }
     end,
   },
   {
